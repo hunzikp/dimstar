7cca2dd2d35c068d0dbfe089735be14302634eb2
Philipp Hunziker <philipp@icr-s04.ethz.ch>
2018-06-25 16:50:16 +0200

    new probit simulation plots

--- scripts/mc_sprobit.R
+++ scripts/mc_sprobit.R
@@ -272,81 +272,71 @@ results.df <- do.call('rbind', lapply(results.ls, function(x) as.data.frame(x)))
 results.df <- cbind(config.df, results.df)
 saveRDS(results.df, file = 'results/mc_sprobit_rmse.rds')
 
+# results.df <- readRDS('results/mc_sprobit_rmse.rds')
+
 
 ###################
 # Plot bias / RMSE
 ###################
 
 library(ggplot2)
+library(data.table)
+library(gridExtra)
 
-## Rho bias
+## Calculate Rho bias
 rho_bias.df <- results.df[results.df$model != 'em',]
-rho_bias.df$rb <- rho_bias.df$rho_est - rho_bias.df$rho
+rho_bias.df$model[rho_bias.df$model == 'dimstar'] <- 'mcem'
+rho_bias.df$value <- rho_bias.df$rho_est - rho_bias.df$rho
 
 rho_bias.df$rho_label <- paste0('rho = ', rho_bias.df$rho)
 rho_bias.df$N_label <- paste0('N = ', rho_bias.df$N)
+rho_bias.df$N_label = factor(rho_bias.df$N_label, levels=unique(rho_bias.df$N_label)[order(unique(rho_bias.df$N_label), decreasing = T)])
 
-p <- ggplot(rho_bias.df, aes(x=model, y=rb)) +  geom_violin()
+## Prepare aggregates
+agg.dt <- data.table(rho_bias.df)
+rmse <- function(x) {sqrt(mean(x^2, na.rm=TRUE))}
+agg.df <- data.frame(agg.dt[, j=list(bias = mean(value, na.rm = TRUE), rm = rmse(value)),by = list(rho_label, N_label, model)])
+
+## Determine y limits
+ylim <- c(min(rho_bias.df$value, na.rm = T) - 0.3, max(rho_bias.df$value, na.rm = T) + 0.3)
+
+p <- ggplot(rho_bias.df, aes(x=model, y=value)) +  geom_violin()
 p <- p + geom_hline(yintercept = 0) + stat_summary(fun.y=mean, geom="point", shape=23, size=2)
 p <- p + facet_grid(rho_label ~ N_label) + xlab("") + ylab("rho bias")
-ggsave(filename = "plots/mc_sprobit_rhobias.pdf", plot = p)
+p <- p + ylim(ylim[1], ylim[2])
+p <- p + geom_text(data=agg.df, aes(x = model, y = ylim[1] + 0.1, 
+                                    label=sprintf("%0.2f", round(bias, digits = 3))), size=4)
+p1 <- p + geom_text(data=agg.df, aes(x = model, y = ylim[2] - 0.1, 
+                                    label=sprintf("%0.2f", round(rm, digits = 3))), size=4)
 
-## Beta bias
+## Calculate Beta bias
 beta_bias.df <- results.df[results.df$model != 'em',]
-beta_bias.df$bb <- beta_bias.df$beta1_est - rho_bias.df$beta1
+beta_bias.df$model[beta_bias.df$model == 'dimstar'] <- 'mcem'
+beta_bias.df$value <- beta_bias.df$beta1_est - beta_bias.df$beta1
 
 beta_bias.df$rho_label <- paste0('rho = ', beta_bias.df$rho)
 beta_bias.df$N_label <- paste0('N = ', beta_bias.df$N)
+beta_bias.df$N_label = factor(beta_bias.df$N_label, levels=unique(beta_bias.df$N_label)[order(unique(beta_bias.df$N_label), decreasing = T)])
 
-p <- ggplot(beta_bias.df, aes(x=model, y=bb)) +  geom_violin()
-p <- p + geom_hline(yintercept = 0) + stat_summary(fun.y=mean, geom="point", shape=23, size=2)
-p <- p + facet_grid(rho_label ~ N_label) + xlab("") + ylab("beta1 bias")
-ggsave(filename = "plots/mc_sprobit_betabias.pdf", plot = p)
-
-
-###################
-# Summarize bias / RMSE
-###################
-
-library(data.table)
-results.dt <- data.table(results.df)
+## Prepare aggregates
+agg.dt <- data.table(beta_bias.df)
 rmse <- function(x) {sqrt(mean(x^2, na.rm=TRUE))}
+agg2.df <- data.frame(agg.dt[, j=list(bias = mean(value, na.rm = TRUE), rm = rmse(value)),by = list(rho_label, N_label, model)])
 
-rho_bias.dt <- results.dt
-rho_bias.dt$rb <- rho_bias.dt$rho_est - rho_bias.dt$rho
-rho_bias.dt[, j=list(bias = mean(rb, na.rm = TRUE), rm = rmse(rb)),by = list(rho, N, model)]
+## Determine y limits
+ylim2 <- c(min(beta_bias.df$value, na.rm = T) - 0.3, max(beta_bias.df$value, na.rm = T) + 0.3)
 
-beta_bias.dt <- results.dt
-beta_bias.dt$rb <- beta_bias.dt$beta1_est - rho_bias.dt$beta1
-beta_bias.dt[, j=list(bias = mean(rb, na.rm = TRUE), rm = rmse(rb)),by = list(rho, N, model)]
+p <- ggplot(beta_bias.df, aes(x=model, y=value)) +  geom_violin()
+p <- p + geom_hline(yintercept = 0) + stat_summary(fun.y=mean, geom="point", shape=23, size=2)
+p <- p + facet_grid(rho_label ~ N_label) + xlab("") + ylab("beta bias")
+p <- p + ylim(ylim2[1], ylim2[2])
+p <- p + geom_text(data=agg2.df, aes(x = model, y = ylim2[1] + 0.1, 
+                                    label=sprintf("%0.2f", round(bias, digits = 3))), size=4)
+p2 <- p + geom_text(data=agg2.df, aes(x = model, y = ylim2[2] - 0.1, 
+                                     label=sprintf("%0.2f", round(rm, digits = 3))), size=4)
 
-
-###################
-# Coverage prob
-###################
-library(data.table)
-
-## Coverage of rho
-rc.df <- results.df[results.df$model != 'em', c('N', 'model', 'rho', 'rho_hi', 'rho_lo')]
-rc.df$rho_hit <- rc.df$rho < rc.df$rho_hi & rc.df$rho > rc.df$rho_lo
-rc.dt <- data.table(rc.df)
-rc.dt <- rc.dt[, .(hit_sum = sum(rho_hit, na.rm = TRUE), hit_trial = sum(!is.na(rho_hit))), by = .(N,model,rho)]
-rc.dt$hit_prop <- rc.dt$hit_sum/rc.dt$hit_trial
-
-rc.dt$lo <- NA
-rc.dt$hi <- NA
-for (i in 1:nrow(rc.dt)) {
-  if (rc.dt$hit_trial[i] > 0) {
-    pt <- prop.test(x = rc.dt$hit_sum[i], n = rc.dt$hit_trial[i])
-    rc.dt$lo[i] <- pt$conf.int[1]
-    rc.dt$hi[i] <- pt$conf.int[2]
-  }
-}
-p <- ggplot(data=rc.dt, aes(model))
-p <- p + geom_bar(aes(weight = hit_prop))
-p <- p + geom_errorbar(aes(ymin=lo, ymax=hi), width = 0.2)
-p <- p + facet_grid(rho ~ N) + geom_hline(yintercept = 0.9, linetype = 2, size = 0.25)
-p
+p <- arrangeGrob(p1, p2, nrow=1, ncol=2) 
+ggsave(filename = "plots/mc_sprobit_bias.pdf", plot = p)
 
 
 
@@ -406,19 +396,21 @@ results.df <- do.call('rbind', lapply(results.ls, function(x) as.data.frame(x)))
 results.df <- cbind(config.df, results.df)
 saveRDS(results.df, file = 'results/mc_sprobit_complexity.rds')
 
+# results.df <- readRDS('results/mc_sprobit_complexity.rds')
 
 ###################
 # Plot Scalability
 ###################
 
 scal.dt <- data.table(results.df[results.df$model != 'em',])
+scal.dt$model[scal.dt$model == 'dimstar'] <- 'mcem'
 scal.dt <- scal.dt[, j=list(elapsed = mean(elapsed, na.rm = TRUE)), by = list(N, model)]
 
 p <- ggplot(data=scal.dt, aes(x=N, y=elapsed, group=model)) +
   geom_line(aes(linetype=model)) +
   geom_point(aes(shape=model)) + xlab("N") + ylab("Estimation Time") +
   theme(legend.position="bottom")
-ggsave(filename = "plots/mc_sprobit_complexity.pdf", plot = p)
+ggsave(filename = "plots/mc_sprobit_complexity.pdf", plot = p, scale = 0.5)
 
 
 
